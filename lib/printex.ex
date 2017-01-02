27447e02dd80396cf8a62355a241640aba8d5df5
blackode <codeme@gmx.com>
2017-01-03 02:19:20 +0530

    Printex version 0.1.0

--- lib/printex.ex
+++ lib/printex.ex
@@ -2,45 +2,223 @@ defmodule Printex do
   alias IO.ANSI,as: Ansi
 
   @moduledoc """
-  Documentation for Printex.
+  Documentation for Printex. 
   --------------------------
-  Printex Module helps you to print the data and strings in color format in console.
-  You use in multiple ways like printing the :error message and many formats.
-  
+  Printex is a Color Printer Module,helps you to print the data and strings in color format in console.
+  You can use in multiple ways like printing the :error message and many formats.
   This helps you identify message from the regular out put of the console. 
-  You can also specify the :bgcolor - Background color for the text to lay on.
+  You can also specify the :bg_color - Background color for the text to lay on.
+  For more examples and screen shots and how to usage check the [github](https://github.com/blackode/printex)
 
   """
   @doc """
   This function prints the string passed in the color you specified or else it uses 
-  the default format for printing i.e regulat format IO.puts 
+  the default format for printing i.e regular format IO.puts
+
 
   ## Examples
 
       iex> Printex.prints("message")
-      "message"
-      :ok
+      This prints the message in default color
       iex> Printex.prints("message",:red)
-      "message"
-      :ok
+      This prints the message in red color
       iex> Printex.prints {"message",:red}
-      "message"
-      :ok
-      iex> Printex.prints (string: "message",color: :red)
-      "message"
-      :ok
+      This prints the message in  red color
+      iex> Printex.prints (%{string: "message",color: :red})
+      This prints the message in  red color
+      iex> Printex.prints ("message",:red,:white)
+      This prints the message in red color with white background
   """
+
+  ####################################
+  #         Public  Definitions      # 
+  ####################################
+
+  #################
+  ##    prints   ##
+  #################
+  
   def prints({string,color}) do
     _prints(string,color)
   end
 
-  def prints (%{string: string, color: color \\ :default}) do
+  def prints(%{string: string, color: color}) do
     _prints(string,color)
   end
   
-  def prints(string,color \\:default) do
+  def prints(string,color \\ :white) do
     _prints(string,color)
   end
+
+  def prints(string,color,bg_color) do
+    case color_check?(color) do
+    false ->
+      print_error "color.t is not matched"
+      print_info "[:black,:blue,:cyan,:green,:magenta,:red,:white,:yellow]"
+    true ->
+      case color_check?(bg_color) do 
+        false -> 
+          print_error "bg_color.t is not matched"
+          print_info "[:black,:blue,:cyan,:green,:magenta,:red,:white,:yellow]"
+        true ->
+          ansi_bgformat(color,bg_color,string)
+      end
+    end
+    
+  end
+
+  
+
+  #################
+  ## print_error ##
+  #################
+
+  @doc """
+  Prints the given message in the error format in the red color
+
+  ## Examples
+
+      iex> Printex.print_error "This is error"
+
+  """
+
+  def print_error(string) when is_binary(string) do 
+    ansi_format(:red,string,"ERROR","error_message")
+    |> IO.puts 
+  end
+
+  #################
+  ## print_info  ##
+  #################
+
+  @doc """
+  Prints the given message in the information format in the green color
+
+  ## Examples
+
+      iex> Printex.print_info "This is info"
+
+  """
+
+  def print_info(string) when is_binary(string) do 
+    ansi_format(:green,string,"INFO","info_message")
+    |> IO.puts 
+  end
+
+  #################
+  # print_warning #
+  #################
+
+  @doc """
+  Prints the given message in the warning format in the yellow color
+
+  ## Examples
+
+      iex> Printex.print_warning "This is warning"
+  """
+  def print_warning(string) when is_binary(string) do 
+    ansi_format(:yellow,string,"WARNING","warn_message")
+    |> IO.puts 
+  end
+
+  @doc """
+  This prints the given text in the format you specified. The string formats are 
+  as follows
+  :x_on_y here {x,y} can be any 9 possible colors
+
+  1. black
+  2. blue
+  3. cyan
+  4. green
+  5. magenta
+  6. red
+  7. white
+  8. yellow     
+
+  In addition you can also specify respective light combination of colors
+
+  ## Usage 
+  `:black_on_white`    
+  prints the text in black color with white background.
+  `:red_on_light_green`          
+  prints the text in red color with light_green background.
+  `:light_blue_on_light_cyan`             
+  prints the text in light_blue color with light_cyan background.
+
+  """
+  @spec color_print(binary,atom)::list
+
+  def color_print(string,format) when is_binary(string) and is_atom(format) do
+    [color,bg_color] = parse_colors(format)
+    prints(string,String.to_atom(color),String.to_atom(bg_color))
+  end
+
+  ##########
+  #  print #
+  ##########
+
+  @doc """
+  Prints the given data with the label provided.If the label is not provided
+  label data is used. How ever you can say not to use the label by passing `:no_label`
+  option
+  ## Examples
+      print("print with out label",:no_label)
+      print("Normal Print")
+      print([1,2,3])
+  ## Usage
+  You can use this function in three different ways.
+  1. Using the default label format.`print("hello")`
+  2. With out any label text. `print("hello",:no_label)`
+  3. Passing label option as string. `print("hello","greeting")` here the text greeting is treated as :label
+  4. Passing label option as list. `print("hello",label: "greeting")`
+  """
+  def print(data,label \\ :default) do 
+    case label do
+      :default ->
+        IO.inspect(data,label: """
+                                  ########
+                                  # data #
+                                  ########
+                               """
+                   )
+      :no_label ->
+        IO.inspect data
+      label when is_binary(label) -> 
+        IO.inspect(data,label: label)
+      label when is_list(label) -> 
+        IO.inspect(data,label)
+      _-> 
+        print_error("case value not matched any")
+    end
+  end
+
+
+  ####################################
+  #         Private Definitions      # 
+  ####################################
+
+  defp parse_colors(atom) when is_atom(atom) do 
+   atom
+   |> Atom.to_string
+   |> String.split(~r{_on_})
+  end
+
+  defp ansi_bgformat(color,bg_color,string) do
+    IO.puts ansi_bgcolor(bg_color) <> ansi_color(color) <> string <> Ansi.reset
+  end
+
+  defp ansi_format(color,string,type,type_message) do
+    Ansi.format([color,""" 
+        ################
+            #{type}   
+        ################
+        -------------------------
+        #{type_message}: #{string}
+        -------------------------
+        """],true)
+  end
+
+  
 
   defp _prints(string,color) do
     [color,string]
@@ -49,15 +227,103 @@ defmodule Printex do
     |> IO.puts
   end
 
-  defp params_validation [color,string]do
+  defp params_validation([color,string]) do # checks the type and possible values
     if is_atom(color) do
-      if is_string(string) do
-        [color,string]
+      if is_binary(string) do
+        case color_check?(color) do
+        false ->  
+          [:red,":error,:color_value color_value ond of[:black,:blue,:cyan,:green,:magenta,:red,:white,:yellow]"]
+        true ->
+          [color,string]
+        end
       else
         [:red,":error,:type String.t should be the :binary"]
       end
     else 
         [:red,":error,:type color.t should be the :atom"]
+    end
+  end
+
+  defp color_check?(color) do
+   colors = [:black,:blue,:cyan,:green,:magenta,:red,:white,:yellow] 
+   Enum.any?(colors,fn(kolor)->kolor==color end)
+  end
+
+  defp ansi_color(color) when is_atom(color) do
+    case color do
+      :cyan ->
+        Ansi.cyan()
+      :light_cyan ->
+        Ansi.light_cyan()
+      :blue ->
+        Ansi.blue()
+      :light_blue ->
+        Ansi.light_blue()
+      :black ->
+        Ansi.black()
+      :light_black ->
+        Ansi.light_black()
+      :yellow ->
+        Ansi.yellow()
+      :light_yellow ->
+        Ansi.light_yellow()
+      :green ->
+        Ansi.green()
+      :light_green ->
+        Ansi.light_green()
+      :magenta ->
+        Ansi.magenta()
+      :light_magenta ->
+        Ansi.light_magenta()
+      :red ->
+        Ansi.red()
+      :light_red ->
+        Ansi.light_red()
+      :white ->
+        Ansi.white()
+      :light_white ->
+        Ansi.light_white()
+        _ ->
+          print_error "color did not matched"
+    end
+  end
+
+  defp ansi_bgcolor(color) when is_atom(color) do
+    case color do
+      :cyan ->
+        Ansi.cyan_background()
+      :light_cyan ->
+        Ansi.light_cyan_background()
+      :blue ->
+        Ansi.blue_background()
+      :light_blue ->
+        Ansi.light_blue_background()
+      :black ->
+        Ansi.black_background()
+      :light_black ->
+        Ansi.light_black_background()
+      :yellow ->
+        Ansi.yellow_background()
+      :light_yellow ->
+        Ansi.light_yellow_background()
+      :green ->
+        Ansi.green_background()
+      :light_green ->
+        Ansi.light_green_background()
+      :magenta ->
+        Ansi.magenta_background()
+      :light_magenta ->
+        Ansi.light_magenta_background()
+      :red ->
+        Ansi.magenta_background()
+      :light_red ->
+        Ansi.light_red_background()
+      :white ->
+        Ansi.white_background()
+      :light_white ->
+        Ansi.light_white_background()
+        _ ->
+          print_error "color did not matched"
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,27 +7,41 @@ defmodule Printex.Mixfile do
      elixir: "~> 1.4-rc",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "A module for checking the type and values of an argument",
+     package: package(),
      deps: deps()]
   end
+
+  def package do
+    [
+      maintainers: [" Blackode","Ahamtech"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/blackode/printex"}
+    ]
+  end
+
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [applications: [:logger,:typex]]
   end
 
   # Dependencies can be Hex packages:
   #
-  #   {:my_dep, "~> 0.3.0"}
+  #   {:mydep, "~> 0.3.0"}
   #
   # Or git/path repositories:
   #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, github: "elixir-lang/ex_doc", override: true,only: :dev},
+     {:earmark, "~> 1.0", only: :dev}, {:dialyxir, "~> 0.3", only: [:dev]},
+     {:typex, "~> 0.1.0"}
+
+   ]
   end
 end
