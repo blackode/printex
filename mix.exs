bd46c8d512c28ea3dc27342e4e6cd04a2e38408d
Ankanna <hi@blackode.in>
2017-11-20 05:24:42 +0530

    removed the typex application dependency

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Printex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger,:typex]]
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
